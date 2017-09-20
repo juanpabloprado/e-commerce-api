bc168a4682755a29b11da2ce57131ba82ab37e1b
Juan Pablo Prado <juan@juanpabloprado.com>
2017-09-20 10:49:05 -0500

    Parse correctly prices from amazon e.g. $400.61 - $1,418.64 to 400.61

--- src/main/java/com/juanpabloprado/ecommerceapi/product/ProductServiceImpl.java
+++ src/main/java/com/juanpabloprado/ecommerceapi/product/ProductServiceImpl.java
@@ -10,13 +10,23 @@ import org.springframework.stereotype.Service;
 public class ProductServiceImpl implements ProductService {
   @Override public Product fromIntegration(Result result) {
     try {
-      Number number = NumberFormat.getCurrencyInstance(new Locale("es", "MX")).parse(result.getPrice());
-      BigDecimal price = new BigDecimal(number.doubleValue());
+      String firstPrice = getFirstWord(result.getPrice());
+      Number number =
+          NumberFormat.getCurrencyInstance(new Locale("es", "MX")).parse(firstPrice);
+      BigDecimal price = new BigDecimal(number.doubleValue()).setScale(2, BigDecimal.ROUND_HALF_EVEN);;
 
       return new Product(result.getName(), price, null, result.getImageUrl());
-    } catch (Exception ex ) {
-      return new Product(result.getName(), new BigDecimal("0.00"), result.getPrice(), result.getImageUrl());
+    } catch (Exception ex) {
+      return new Product(result.getName(), new BigDecimal("0.00"), result.getPrice(),
+          result.getImageUrl());
     }
+  }
 
+  private String getFirstWord(String text) {
+    if (text.indexOf(' ') > -1) {
+      return text.substring(0, text.indexOf(' '));
+    } else {
+      return text;
+    }
   }
 }
